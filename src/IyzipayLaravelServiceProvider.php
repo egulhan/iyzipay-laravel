4189f708fbbe978f6ca7fdf5754dad0f20a5cd0c
Erman Gülhan <mr.ermangulhan@gmail.com>
2019-05-18 08:15:01 +0300

    published migrations create_payment_logs and create_threeds_payment_step_logs

--- src/IyzipayLaravelServiceProvider.php
+++ src/IyzipayLaravelServiceProvider.php
@@ -48,6 +48,18 @@ class IyzipayLaravelServiceProvider extends ServiceProvider
             ], 'migrations');
         }
 
+        if (!class_exists('CreatePaymentLogsTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_payment_logs_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_payment_logs_table.php'),
+            ], 'migrations');
+        }
+
+        if (!class_exists('CreateThreedsPaymentStepLogsTable')) {
+            $this->publishes([
+                __DIR__ . '/../database/migrations/create_threeds_payment_step_logs_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . 'create_threeds_payment_step_logs_table.php'),
+            ], 'migrations');
+        }
+
         $this->app->booted(function () {
             $schedule = $this->app->make(Schedule::class);
             $schedule->command('iyzipay:subscription_charge')->daily();
